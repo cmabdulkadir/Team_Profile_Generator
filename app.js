0884965f420a33defa3b26b61191e92ea5c7ddba
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-02-01 21:14:24 -0600

    added questions for engineer and intern

--- app.js
+++ app.js
@@ -1,49 +1,152 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
 
-function selectRole(){
+// import classes here
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+
+
+// import page-template
+const generateHTML = require('./src/page-template');
+
+// global variable for team array
+let myTeam = [];
+
+function selectRole() {
     inquirer.prompt([{
         type: "list",
-        message:"What role would you like to add?",
+        message: "What role would you like to add?",
         name: "chosenRole",
-        choices:["Manager", "Engineer", "Intern", "none"],
-     
-     }]).then((answer)=>{
-         console.log(answer.chosenRole)
-     })
-
+        choices: ["Manager", "Engineer", "Intern", "none"],
+    }]).then((answer) => {
+        console.log(answer.chosenRole)
+        if (answer.chosenRole === "Manager") {
+            createManager()
+        } else if(answer.chosenRole === 'Engineer') {
+            createEngineer()
+        } else if(answer.chosenRole === 'Intern') {
+            createIntern()
+        } else {
+            generateTeam(myTeam)
+        }
+    })
 }
 
-function createManager(){
+function createManager() {
     inquirer.prompt([
         {
-        type: "input",
-        message:"What is the manager's name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message:"What is the manager's id?",
-        name: "id",
-        
+            type: "input",
+            message: "What is the manager's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is the manager's id?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officeNumber",
+        },
+    ]).then((answer) => {
+        // create a new manager
+        const newManager = new Manager(answer.name,answer.id,answer.email,answer.officeNumber);
 
-    },
-    {
-        type: "input",
-        message:"What is the manager's email?",
-        name: "email",
+        // add the newly created manager to the team array
+        myTeam.push(newManager);
 
-    },
-    {
-        type: "input",
-        message:"What is the manager's office number?",
-        name: "officeNumber", 
+        console.log(myTeam)
 
-    },
+        // ask again which role
+        selectRole();
 
-
-]).then((answer)=>{
-    console.log(answer)
-})
-    
+    })
 }
-createManager()
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's id?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is the engineer's github?",
+            name: "github",
+        },
+    ]).then((answer) => {
+        // create a new manager
+        const newEngineer = new Engineer(answer.name,answer.id,answer.email,answer.github);
+
+        // add the newly created manager to the team array
+        myTeam.push(newEngineer);
+
+        console.log(myTeam)
+
+        // ask again which role
+        selectRole();
+
+    })
+}
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is the intern's id?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is the intern's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is school is the intern enrolled to?",
+            name: "school",
+        },
+    ]).then((answer) => {
+        
+        const newIntern = new Intern(answer.name,answer.id,answer.email,answer.school);
+
+        // add the newly created intern to the team array
+        myTeam.push(newIntern);
+
+        console.log(myTeam)
+
+        // ask again which role
+        selectRole();
+
+    })
+}
+
+// function for generating the HTML file
+function generateTeam (teamArray) {
+    const generatedOutput = generateHTML(teamArray);
+
+    fs.writeFileSync('./output/team.html', generatedOutput);
+}
+
+selectRole()
